2b422ef4564da643bf6910f4eec1953904c22a6b
Greg Schwimer <schwim@bitrail.com>
2023-04-12 13:37:52 -0700

    cleanup

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 echo "Installing zsh and friends..."
 if [[ `uname` == "Linux" ]] ; then
 	sudo apt update
-	sudo apt install --yes zsh wget ripgrep bat git snapd curl \
+	sudo apt install --yes zsh wget ripgrep bat git curl \
 		build-essential gcc make unzip \
 		gdb lcov pkg-config \
 		libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
